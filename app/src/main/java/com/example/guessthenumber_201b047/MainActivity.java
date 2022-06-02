566752ca89ae03d2d80e505d39795d752aa439c0
Anjali <anjalitiwarijpguna@gmail.com>
2022-06-02 21:06:09 +0530

    initial commit

---
+++ app/src/main/java/com/example/guessthenumber_201b047/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.guessthenumber_201b047;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button buttonL;
+    Button buttonR;
+    DataModel model;
+    int points = 0;
+    TextView result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        model = new DataModel();
+        buttonL = findViewById(R.id.buttonL);
+        buttonL.setText(Integer.toString(model.getNum1()));
+        buttonR = findViewById(R.id.buttonR);
+        buttonR.setText(Integer.toString(model.getNum2()));
+        result = findViewById(R.id.numberOfPoints);
+    }
+
+    public void onClick(View view) {
+        if (points == 5) {
+            Toast.makeText(this, "you win the game", Toast.LENGTH_LONG).show();
+            points = 0;
+        } else {
+            Button temp = (Button) view;
+            String txt = (String) temp.getText();
+            String answer = Integer.toString(model.getAnswer());
+            if (txt.equals(answer)) {
+                Toast.makeText(this, "CORRECT", Toast.LENGTH_LONG).show();
+                points++;
+                result.setText(" Points" + points);
+            } else {
+                Toast.makeText(this, "NOT CORRECT", Toast.LENGTH_LONG).show();
+                points--;
+                result.setText(" Points" + points);
+            }
+            model.setNum1();
+            while(true)
+            {
+                model.setNum2();
+                if(model.getNum1()!=model.getNum2())
+                    break;
+            }
+            model.setAnswer();
+            buttonL.setText(Integer.toString(model.getNum1()));
+            buttonR.setText(Integer.toString(model.getNum2()));
+
+
+        }
+    }
+}
